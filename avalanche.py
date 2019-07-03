bfab2a27267912f3f57b28561cca48a0f86aaa94
Iceman <Michael.Neuhauser@bfw.gv.at>
2019-07-03 15:11:24 +0200

    - sort start cells by altitude - with check if release is in runout, then take it out for further calulation

--- avalanche.py
+++ avalanche.py
@@ -14,32 +14,28 @@ import rasterio
 import sys
 import time
 
-class cell():
+sys.path.append('/home/neuhauser/Documents/Milan')
+import raster_io as io
+
+
+class Cell():
+
     
-    def __init__(self, rowindex, colindex, altitude, mass):
+    def __init__(self, rowindex, colindex, altitude, velocity_sqr, mass):
         self.rowindex = rowindex
         self.colindex = colindex
         self.altitude = altitude
         self.mass = mass
-        self.velocity = 0
+        self.velocity_sqr = velocity_sqr
         self.cellsize = 10
         self.exp = 1
-        self.alpha = 50
-                       
-    def calc_velocity(self, startcell):
-        dx = np.abs(startcell.colindex - self.colindex) * self.cellsize
-        dy = np.abs(startcell.rowindex - self.rowindex) * self.cellsize
-        dh = np.sqrt(dx**2 + dy**2)
-        temp = startcell.altitude - self.altitude - dh * np.tan(np.deg2rad(self.alpha))
-        if temp > 0:
-            self.velocity = np.sqrt(2*9.81*temp)
-        else:
-            self.velocity = 0
-            
-        return self.velocity
-        
-    def add_mass(self, mass):
-        self.mass += mass
+        self.alpha = 5
+        self.mu_l = np.tan(np.deg2rad(30))
+        #self.mu_l = 0
+        self.mu_g = np.tan(np.deg2rad(5))
+        self.vel_sqr_out = np.zeros((3,3))
+        self.kin = np.zeros((3,3))
+
         
     def calc_tanbeta(self, dem_ng):       
         self.tan_beta = np.zeros((3, 3))
@@ -48,20 +44,21 @@ class cell():
             for j in range(-1, 2):
                 row_idx = self.rowindex + i
                 col_idx = self.colindex + j
-
+    
                 if row_idx == self.rowindex or col_idx == self.colindex:
                     distance = cellsize
                 else:
                     distance = cellsize * math.sqrt(2)
                 self.tan_beta[i+1, j+1] = (dem_ng[i+1, j+1] - self.altitude)/distance
+
                 
-    def calc_distribution(self, dem_ng):      
+    def calc_mass(self, dem_ng):      
         self.calc_tanbeta(dem_ng)
-        self.mass_dist = np.zeros((3,3))
+        self.mass_dist = np.zeros((3, 3))
         exp = self.exp
         mass_threshold = 0.0001
 
-        self.tan_beta[self.tan_beta > 0] = 0
+        self.tan_beta[self.vel_sqr_out < 0] = 0
         for i in range(3):
             for j in range(3):
                 self.mass_dist[i, j] = self.mass * self.tan_beta[i, j] ** exp / np.sum(self.tan_beta ** exp)
@@ -70,42 +67,57 @@ class cell():
         out_col = self.colindex - 1 + out_col_local
         mass_list = self.mass_dist[out_row_local, out_col_local]
         
-        return out_row, out_col, mass_list
+                       
+    def calc_velocity(self, startcell, dem_ng):
+        g = 9.81
+        v_sqr = np.zeros((3, 3))
+        
+        for i in range(-1, 2):
+            for j in range(-1, 2):
+                if not (i == 0 and  j == 0):
+                    dx = np.abs(startcell.colindex - self.colindex + j) * self.cellsize
+                    dy = np.abs(startcell.rowindex - self.rowindex + i) * self.cellsize
+                    ds_global = np.sqrt(dx**2 + dy**2)
+                    #print(dx, dy, ds_global,i,j)
+                    dx = np.abs(j) * self.cellsize
+                    dy = np.abs(i) * self.cellsize
+                    ds_local = np.sqrt(dx**2 + dy**2)
+                    #print(dx, dy, ds_local)
+                    
+                    v_sqr_local = self.velocity_sqr + 2 * g * ((self.altitude - dem_ng[i+1, j+1]) - self.mu_l * ds_local)
+                    v_sqr_global = 2 * g*((startcell.altitude - dem_ng[i+1, j+1]) - self.mu_g * ds_global)
+                    
+                    #print('Local: ' + str(v_sqr_local) + '  Global:' + str(v_sqr_global))
+       
+                    v_sqr[i+1, j+1] = v_sqr_global
+                    self.vel_sqr_out = v_sqr
+            
+            self.calc_mass(dem_ng)
+            self.kin = 0.5 * self.mass_dist * self.vel_sqr_out
+       
+        #v_sqr[v_sqr < 0] = 0
+        return v_sqr, kin
+
+        
+    def add_mass(self, mass):
+        self.mass += mass
     
-    
-def read_header(input_file):
+####CORE
 
-    raster = rasterio.open(input_file)
-    if raster is None:
-        print('Unable to open {}'.format(input_file))
-        sys.exit(1)
+#Read input data
+#file = 'Fonnbu_dhm.asc'
+path = '/home/neuhauser/Documents/Milan/'
+file = path + 'r_dem.tif'
+release_file = path + 'r_src.tif'
+#mass_out = 'Mass_exp1.asc'
+vel_out =path + 'Velocity_v3.asc'
+dem, header = io.read_raster(file)     
+release, header = io.read_raster(release_file) 
 
-    header = {}
-    header['ncols'] = raster.width
-    header['nrows'] = raster.height
-    header['xllcorner'] = (raster.transform * (0, 0))[0]
-    header['yllcorner'] = (raster.transform * (0, raster.height))[1]
-    header['cellsize'] = raster.transform[0]
-    header['noDataValue'] = raster.nodata
-    return header
-
-
-def read_raster(input_file):
-
-    header = read_header(input_file)
-    raster = rasterio.open(input_file)
-    my_array = raster.read(1)
-
-    return my_array, header
-
-
-file = 'Fonnbu_dhm.asc'
-mass_out = 'Mass_exp8.asc'
-vel_out = 'Velocity.asc'
-dem, header = read_raster(file)   
-#dem = np.ones((100, 100), dtype=np.int)
+#Initialize Output raster
 mass_dist = np.zeros_like(dem)
-vel = np.zeros_like(dem)
+vel_sqr_global = np.zeros_like(dem)
+kin_energy = np.zeros_like(dem)
 
 #for i in range(10):
 #    dem[:,i] = dem[:,i] * (10-i)
@@ -113,39 +125,109 @@ vel = np.zeros_like(dem)
 start = time.time()
 
 cell_list = []  
-startcell = cell(500, 1, dem[500, 1], 1)
-#mass_dist[startcell.rowindex, startcell.colindex] = 1
-
+#startcell = Cell(500, 1, dem[500, 1], 0)
+row_idx, col_idx = np.where(release == 1)
+row_idx = row_idx[0]
+col_idx = col_idx[0]
+startcell = Cell(row_idx, col_idx, dem[row_idx, col_idx], 1, 1)
 cell_list.append(startcell)
-row, col, mass = startcell.calc_distribution(dem[startcell.rowindex-1:startcell.rowindex+2,startcell.colindex-1:startcell.colindex+2])
-for k in range(len(row)): 
-    cell_list.append(cell(row[k], col[k], dem[row[k], col[k]], mass[k]))
-
-for cells in cell_list:
-    if not cells == startcell:
-        vel[cells.rowindex, cells.colindex] = cells.calc_velocity(startcell)
-        if cells.velocity > 0:
-            print(cells.velocity)
-            row, col, mass = cells.calc_distribution(dem[cells.rowindex-1:cells.rowindex+2,cells.colindex-1:cells.colindex+2])
+# ToDO: set ng_cells for array!!!
+#ng_cells = startcell.rowindex-1:startcell.rowindex+2,startcell.colindex-1:startcell.colindex+2
+# =============================================================================
+# dem_ng = dem[startcell.rowindex-1:startcell.rowindex+2,startcell.colindex-1:startcell.colindex+2]
+# velocity_sqr = startcell.calc_velocity(startcell, dem_ng)z i − 2z i+1
+# for i in range(-1, 2):
+#     for j in range(-1, 2):
+#         if velocity_sqr[i+1, j+1] > vel_sqr[startcell.rowindex+i,startcell.colindex+j]:
+#             vel_sqr[startcell.rowindex+i,startcell.colindex+j] = velocity_sqr[i+1, j+1]
+# =============================================================================
+        
             
-            for i in range(len(cell_list)): #Taking out multiple cells, ToDo: add velocity
-                j = 0
-                while j < len(row):
-                    if row[j] == cell_list[i].rowindex and col[j] == cell_list[i].colindex:
-                        cell_list[i].add_mass(mass[j])
-                        row = np.delete(row, j)
-                        col = np.delete(col, j)
-                        mass = np.delete(mass, j)
-                    else:
-                        j += 1
-                        
-            for k in range(len(row)):             
-                    cell_list.append(cell(row[k], col[k], dem[row[k], col[k]], mass[k]))
-                
-            mass_dist[cells.rowindex, cells.colindex] = cells.mass
-        else:
-            break
-    #vel[cells.rowindex, cells.colindex] = cells.calc_velocity(startcell)
+#vel[ng_cells] = velocity
+
+#mass_dist[startcell.rowindex, startcell.colindex] = 1
+exist = False
+
+
+for cell in cell_list:
+    dem_ng = dem[cell.rowindex-1:cell.rowindex+2,cell.colindex-1:cell.colindex+2]
+    velocity_sqr_local, kin = cell.calc_velocity(cell, dem_ng)
+    row = []
+    col = []
+    for i in range(-1, 2):
+        for j in range(-1, 2):
+            if velocity_sqr_local[i+1, j+1] > vel_sqr_global[cell.rowindex+i,cell.colindex+j]: # add velocity_sqr to global velocity_sqr raster
+                vel_sqr_global[cell.rowindex+i,cell.colindex+j] = velocity_sqr_local[i+1, j+1]
+                row.append(cell.rowindex+i)
+                col.append(cell.colindex+j)
+
+    for i in range(len(cell_list)): #Taking out multiple cells, ToDo: add velocity
+         j = 0
+         while j < len(row):
+             if row[j] == cell_list[i].rowindex and col[j] == cell_list[i].colindex:
+                 cell_list[i].add_mass(mass[j])
+                 cell_list[i].velocity_sqr = max(cell_list[i].velocity_sqr, vel_sqr[row[j], col[j]])
+                 if cell_list[i].velocity_sqr < vel_sqr_global[row[j], col[j]]:
+                     cell_list[i].velocity_sqr = vel_sqr_global[row[j], col[j]]
+                     
+                 row = np.delete(row, j)
+                 col = np.delete(col, j)
+                 #mass = np.delete(mass, j)
+             else:
+                 j += 1
+
+    for k in range(len(row)):
+             cell_list.append(Cell(row[k], col[k], dem[row[k], col[k]], vel_sqr_global[row[k], col[k]]))
+#
+# """
+#                 k = 0
+#
+#                 while k < len(cell_list): #CHeck for multiple cells
+#                     bug = cell.rowindex+i
+#                     bug1 = cell_list[k].rowindex
+#                     bug2 = cell.colindex+j
+#                     bug3 = cell_list[k].colindex
+#                     if (cell.rowindex+i == cell_list[k].rowindex and cell.colindex+j == cell_list[k].colindex):
+#                             #if cell_list[k].velocity_sqr < vel_sqr[cell.rowindex+i,cell.colindex+j]:
+#                         cell_list[k].velocity_sqr = max(cell_list[k].velocity_sqr,vel_sqr[cell.rowindex+i,cell.colindex+j])
+#                         k = len(cell_list)
+#                         exist = True
+#                     else:
+#                         #cell_list.append(Cell(cell.rowindex+i, cell.colindex+j, dem[cell.rowindex+i, cell.colindex+j], velocity_sqr[i+1, j+1]))
+#                         k +=1
+#                 if not exist:
+#                     cell_list.append(Cell(cell.rowindex + i, cell.colindex + j, dem[cell.rowindex + i, cell.colindex + j], velocity_sqr[i + 1, j + 1]))
+# """
+#
+
+
+# =============================================================================
+# row, col, mass = startcell.calc_distribution(dem[startcell.rowindex-1:startcell.rowindex+2,startcell.colindex-1:startcell.colindex+2])
+# for k in range(len(row)): 
+#     cell_list.append(Cell(row[k], col[k], dem[row[k], col[k]], mass[k]))
+# 
+# for cells in cell_list:
+#     if not cells == startcell:
+#         cells.calc_velocity(startcell)
+#         vel[cells.rowindex, cells.colindex] = cells.velocity
+#         mass_dist[cells.rowindex, cells.colindex] = cells.mass
+#         if cells.velocity > 0:
+#             row, col, mass = cells.calc_distribution(dem[cells.rowindex-1:cells.rowindex+2,cells.colindex-1:cells.colindex+2])
+#             
+#             for i in range(len(cell_list)): #Taking out multiple cells, ToDo: add velocity
+#                 j = 0
+#                 while j < len(row):
+#                     if row[j] == cell_list[i].rowindex and col[j] == cell_list[i].colindex:
+#                         cell_list[i].add_mass(mass[j])
+#                         row = np.delete(row, j)
+#                         col = np.delete(col, j)
+#                         mass = np.delete(mass, j)
+#                     else:
+#                         j += 1
+#                         
+#             for k in range(len(row)):             
+#                     cell_list.append(Cell(row[k], col[k], dem[row[k], col[k]], mass[k]))
+# =============================================================================
 
 end = time.time()            
 print(end - start)
@@ -161,11 +243,14 @@ print(end - start)
 
     
 raster_trans = rasterio.open(file)
-new_dataset = rasterio.open(mass_out, 'w', driver='GTiff', height = mass_dist.shape[0], width = mass_dist.shape[1], count=1,  dtype = mass_dist.dtype, crs='+proj=latlong', transform=raster_trans.transform)
-new_dataset.write(mass_dist, 1)
-new_dataset.close()    
-
-new_dataset = rasterio.open(vel_out, 'w', driver='GTiff', height = mass_dist.shape[0], width = mass_dist.shape[1], count=1,  dtype = mass_dist.dtype, crs='+proj=latlong', transform=raster_trans.transform)
-new_dataset.write(vel, 1)
+crs = rasterio.crs.CRS.from_epsg(3912)
+# =============================================================================
+# new_dataset = rasterio.open(mass_out, 'w', driver='GTiff', height = mass_dist.shape[0], width = mass_dist.shape[1], count=1,  dtype = mass_dist.dtype, crs='+proj=latlong', transform=raster_trans.transform)
+# new_dataset.write(mass_dist, 1)
+# new_dataset.close()    
+# =============================================================================
+vel_sqr_global = np.sqrt(vel_sqr_global)
+new_dataset = rasterio.open(vel_out, 'w', driver='GTiff', height = mass_dist.shape[0], width = mass_dist.shape[1], count=1,  dtype = mass_dist.dtype, crs=crs, transform=raster_trans.transform)
+new_dataset.write(vel_sqr_global, 1)
 new_dataset.close()    
 
--- avalanche_distribution_energy.py
+++ avalanche_distribution_energy.py
@@ -20,7 +20,6 @@ class Cell():
         else:            
             self.startcell = startcell # set is_satrt to True
             self.is_start = False # set is_satrt to False
-
         self.calc_energy()
         
     def calc_energy(self):
@@ -30,8 +29,7 @@ class Cell():
             dx = np.abs(self.startcell.colindex - self.colindex) * self.cellsize
             dy = np.abs(self.startcell.rowindex - self.rowindex) * self.cellsize
             ds = np.sqrt(dx**2 + dy**2)
-            dz = self.startcell.altitude - self.altitude
-            
+            dz = self.startcell.altitude - self.altitude            
             self.elh = dz - ds * np.tan(np.deg2rad(self.alpha))
         
     def steepest_descend(self):
@@ -63,50 +61,54 @@ class Cell():
                 self.dist[i, j] = self.tan_beta[i, j] ** exp / np.sum(self.tan_beta ** exp)
         row_local, col_local = np.where(self.dist > threshold)
         return self.rowindex - 1 + row_local, self.colindex - 1 + col_local
+
+    
+def get_start_idx(release):
+    row_list, col_list = np.where(release == 1)  # Gives back the indices of the release areas
+    altitude_list = []
+    for i in range(len(row_list)):
+        altitude_list.append(dem[row_list[i], col_list[i]])    
+    altitude_list, row_list, col_list = list(zip(*sorted(zip(altitude_list, row_list, col_list), reverse=True)))  #Sort this lists by altitude
+    return row_list, col_list    
         
 #Reading in the arrays
 path = '/home/P/Projekte/18130-GreenRisk4Alps/Simulation/PAR6_ValsGries_AUT/'
 file = path + 'dhm_cropped.asc'
 release_file = path + 'init/init_ava_c1.asc'
-ava_out = path + 'distribution_gires.asc'
-elh_out = path + 'energy_gries.asc'
+ava_out = path + 'distribution_gries_v2.asc'
+elh_out = path + 'energy_gries_v2.asc'
 
 dem, header = io.read_raster(file)
 cellsize = header["cellsize"]   
 release, header = io.read_raster(release_file) 
-ava_steep = np.zeros_like(dem)
 elh = np.zeros_like(dem)
 
 start = time.time()
 
-
 cell_list = []  
-row_list, col_list = np.where(release == 1)
+row_list, col_list = get_start_idx(release)
 
-a = list(range(0, len(row_list)))
-
-for i in a:
-    print("Calculating Startcell: " + str(i) + " from " + str(a[-1]))
+startcell_idx = 0
+while startcell_idx < len(row_list):
+    print("Calculating Startcell: " + str(startcell_idx) + " of " + str(len(row_list)) + " = " + str(round(startcell_idx/len(row_list)*100,2)) + "%")
     cell_list = []
-    row_idx = row_list[i]
-    col_idx = col_list[i]
-    
-    ava_steep[row_idx, col_idx] = 1
-    
-    dem_ng = dem[row_idx - 1 : row_idx + 2, col_idx - 1 : col_idx + 2]
+    row_idx = row_list[startcell_idx]
+    col_idx = col_list[startcell_idx]    
+    dem_ng = dem[row_idx - 1:row_idx + 2, col_idx - 1:col_idx + 2] # neighbourhood DEM
     if np.size(dem_ng) < 9:
         continue
     
     startcell = Cell(row_idx, col_idx, dem[row_idx, col_idx], dem_ng, cellsize, startcell=True)
+    # If this is a startcell just give a Bool to startcell otherwise the object startcell
     
     cell_list.append(startcell)
     checked = 0
     for cells in cell_list:
-        if cells.elh < 0: #Stopping Condition
+        if cells.elh < 0:
             continue
         
         row, col = cells.calc_distribution()
-        for i in range(checked, len(cell_list)):
+        for i in range(checked, len(cell_list)):  # Check if Cell already exists
             k = 0
             while k < len(row):
                 if row[k] == cell_list[i].rowindex and col[k] == cell_list[i].colindex:
@@ -116,18 +118,17 @@ for i in a:
                     k += 1
                     
         for k in range(len(row)):
-            dem_ng = dem[row[k]-1:row[k]+2,col[k]-1:col[k]+2]
-            if np.size(dem_ng) < 9:
+            dem_ng = dem[row[k]-1:row[k]+2, col[k]-1:col[k]+2]  # neighbourhood DEM
+            if np.size(dem_ng) < 9:  # Dirty way to don´t care about the edge of the DEM
                 continue
-            cell_list.append(Cell(row[k], col[k], dem[row[k], col[k]], dem_ng, cellsize, cell_list[0]))
-            ava_steep[row[k], col[k]] += 1
-        checked += 1 
-        
+            cell_list.append(Cell(row[k], col[k], dem[row[k], col[k]], dem_ng, cellsize, cell_list[0]))            
+        checked += 1         
         elh[cells.rowindex, cells.colindex] = max(elh[cells.rowindex, cells.colindex], cells.elh)
+    release[elh > 0] = 0  # Check if i hited a release Cell, if so set it to zero and get again the indexes of release cells
+    row_list, col_list = get_start_idx(release)
+    startcell_idx += 1
         
-                
 end = time.time()            
 print('Time needed: ' + str(end - start) + ' seconds')
 
-io.output_raster(file, ava_out, ava_steep)
 io.output_raster(file, elh_out, elh)
