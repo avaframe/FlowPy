a7ef80e979b73b6d917336c8edaad622ff10e574
Iceman <Michael.Neuhauser@bfw.gv.at>
2019-07-31 15:29:07 +0200

    added altitude change of center cell, dz, dependent on Kinetic Energy

--- avalanche_distribution_energy.py
+++ avalanche_distribution_energy.py
@@ -16,8 +16,9 @@ class Cell():
         self.tan_beta = np.zeros_like(self.dem_ng)
         self.dist = np.zeros_like(self.dem_ng)
         self.direction = np.zeros_like(self.dem_ng)
+        self.p_fd = np.zeros_like(self.dem_ng)
         self.alpha = 25
-        self.exp = 8
+        self.exp = 4 # Exp should be a odd number, otherwise in tan_beta negative values will get positive
         self.mass_threshold = 3*10**-4
         #self.velocity_sqr = 0
         self.mass = mass
@@ -33,8 +34,9 @@ class Cell():
             self.is_start = False # set is_satrt to False        
         
         self.calc_kinetic_energy()
-        self.calc_tanbeta()
+        
         self.calc_direction()
+        self.calc_tanbeta()
 
         
     def calc_kinetic_energy(self):
@@ -43,27 +45,33 @@ class Cell():
         e_friction = ds * self.cellsize * np.tan(np.deg2rad(self.alpha))
         self.kin_energy_neighbour = self.kin_e + delta_e_kin_pot - e_friction
         self.kin_energy_neighbour[self.kin_energy_neighbour < 0] = 0
-        
-            
+                    
     def add_mass(self, mass):
         self.mass += mass
         
     def add_parent(self, parent):
         self.parent.append(parent)
-        
-    
-    def calc_tanbeta(self):        
+           
+    def calc_tanbeta(self):  
+        exp = self.exp
+        dh = self.kin_e/(9.81*self.mass*self.cellsize**2 * 2 * 100)
+        print(dh)
+        ds = np.array([[np.sqrt(2),1,np.sqrt(2)],[1,0,1],[np.sqrt(2),1,np.sqrt(2)]])
+        distance = ds * self.cellsize
+        #self.tan_beta = ((self.dem_ng - self.altitude) * (-1)) / distance
         for i in range(3):
             for j in range(3):
-                if i == 1 or j == 1:
-                    distance = self.cellsize
-                else:
-                    distance = np.sqrt(2) * self.cellsize
-                self.tan_beta[i, j] = (self.dem_ng[i, j] - self.altitude) * (-1)/distance
-            #self.tan_beta[self.tan_beta > 0] = 0
+                #if ((self.dem_ng[i, j] - (self.altitude + dh)) * (-1)) > 0:
+                self.tan_beta[i, j] = (self.dem_ng[i, j] - (self.altitude + dh)) * (-1)/distance[i, j]  # Downslope = postive value
+                #else:
+                #    self.tan_beta[i, j] = 0.0637 * np.arctan(distance/((self.dem_ng[i, j] - (self.altitude + dh)) * (-1))) + 0.1
+            self.tan_beta[self.tan_beta < 0] = 0
         self.tan_beta[self.kin_energy_neighbour <= 0] = 0
+        self.tan_beta[self.direction <= 0] = 0
         self.tan_beta[1,1] = 0
-        self.tan_beta = np.abs(self.tan_beta)
+        #self.tan_beta = np.abs(self.tan_beta)
+        if np.sum(self.tan_beta > 0):
+            self.p_fd = self.tan_beta ** exp/ np.sum(self.tan_beta ** exp)
         
     def calc_direction(self):
         if self.is_start:
@@ -71,59 +79,70 @@ class Cell():
         elif self.parent[0].is_start:
             self.direction += 1
         else:
-            kin_e_sum = 0
+            #kin_e_sum = 0
             for parent in self.parent:
                 dx = (parent.colindex - self.colindex) * -1 +1
                 dy = (parent.rowindex - self.rowindex) * -1 +1
+                maxweight = 1
                 if dx == 0 and dy == 0:
-                    self.direction[0, 0] += parent.kin_e * parent.mass
-                    self.direction[1, 0] += parent.kin_e * 0.707 * parent.mass
-                    self.direction[0, 1] += parent.kin_e * 0.707 * parent.mass
+                    self.direction[0, 0] += maxweight
+                    self.direction[1, 0] += 0.707
+                    self.direction[0, 1] += 0.707
                 elif dx == 1 and dy == 0:
-                    self.direction[0, 1] += parent.kin_e * parent.mass
-                    self.direction[0, 0] += parent.kin_e * 0.707 * parent.mass
-                    self.direction[0, 2] += parent.kin_e * 0.707 * parent.mass
+                    self.direction[0, 1] += maxweight
+                    self.direction[0, 0] += 0.707
+                    self.direction[0, 2] += 0.707
                 elif dx == 2 and dy == 0:
-                    self.direction[0, 2] += parent.kin_e * parent.mass
-                    self.direction[0, 1] += parent.kin_e * 0.707 * parent.mass
-                    self.direction[1, 2] += parent.kin_e * 0.707 * parent.mass
+                    self.direction[0, 2] += maxweight
+                    self.direction[0, 1] += 0.707
+                    self.direction[1, 2] += 0.707
                 elif dx == 0 and dy == 1:
-                    self.direction[1, 0] += parent.kin_e * parent.mass
-                    self.direction[2, 0] += parent.kin_e * 0.707 * parent.mass
-                    self.direction[0, 0] += parent.kin_e * 0.707 * parent.mass
+                    self.direction[1, 0] += maxweight
+                    self.direction[2, 0] += 0.707
+                    self.direction[0, 0] += 0.707
                 elif dx == 2 and dy == 1:
-                    self.direction[1, 2] += parent.kin_e * parent.mass
-                    self.direction[0, 2] += parent.kin_e * 0.707 * parent.mass
-                    self.direction[2, 2] += parent.kin_e * 0.707 * parent.mass
+                    self.direction[1, 2] += maxweight
+                    self.direction[0, 2] += 0.707
+                    self.direction[2, 2] += 0.707
                 elif dx == 0 and dy == 2:
-                    self.direction[2, 0] += parent.kin_e * parent.mass
-                    self.direction[1, 0] += parent.kin_e * 0.707 * parent.mass
-                    self.direction[2, 1] += parent.kin_e * 0.707 * parent.mass
+                    self.direction[2, 0] += maxweight
+                    self.direction[1, 0] += 0.707
+                    self.direction[2, 1] += 0.707
                 elif dx == 1 and dy == 2:
-                    self.direction[2, 1] += parent.kin_e * parent.mass
-                    self.direction[2, 0] += parent.kin_e * 0.707 * parent.mass
-                    self.direction[2, 2] += parent.kin_e * 0.707 * parent.mass
+                    self.direction[2, 1] += maxweight
+                    self.direction[2, 0] += 0.707
+                    self.direction[2, 2] += 0.707
                 elif dx == 2 and dy == 2:
-                    self.direction[2, 2] += parent.kin_e * parent.mass
-                    self.direction[2, 1] += parent.kin_e * 0.707 * parent.mass
-                    self.direction[1, 2] += parent.kin_e * 0.707 * parent.mass
+                    self.direction[2, 2] += maxweight
+                    self.direction[2, 1] += 0.707
+                    self.direction[1, 2] += 0.707
 
-                kin_e_sum += parent.kin_e * parent.mass
+                #kin_e_sum += parent.kin_e
             np.rot90(self.direction,2)
-            self.direction /= kin_e_sum
+            #self.direction /= kin_e_sum
             
                     
     def calc_distribution(self):
-        exp = self.exp
+        #exp = self.exp
         threshold = self.mass_threshold
-        
-        for i in range(3):
-            for j in range(3):
-                self.dist[i, j] = self.direction[i, j] * self.tan_beta[i, j] ** exp / np.sum(self.direction * self.tan_beta ** exp) * self.mass
+        if np.sum(self.p_fd > 0):
+            for i in range(3):
+                for j in range(3):
+                    self.dist[i, j] = self.direction[i, j] * self.p_fd[i, j] / np.sum(self.direction * self.p_fd) * self.mass
+                #self.dist = (self.direction * self.kin_energy_neighbour)/np.sum(self.direction * self.kin_energy_neighbour) * self.mass
+                #print(self.direction[i, j] * self.tan_beta[i, j] ** exp)
+                #print(np.sum(self.direction * self.tan_beta ** exp) * self.mass)
+                #print(self.direction[i, j] * self.tan_beta[i, j] ** exp/np.sum(self.direction * self.tan_beta ** exp) * self.mass)
                 
-        count = len(self.dist[self.dist < threshold])
+        #count = len(self.dist[0 < self.dist < threshold])
+        count = ((0 < self.dist) & (self.dist < threshold)).sum()
         mass_to_distribute = np.sum(self.dist[self.dist < threshold])
-        self.dist[self.dist > threshold] += mass_to_distribute / count
+        if mass_to_distribute > 0 and count > 0:
+            self.dist[self.dist > threshold] += mass_to_distribute / count
+            self.dist[self.dist < threshold] = 0
+        if np.sum(self.dist) < self.mass and count > 0:
+            self.dist[self.dist > threshold] += (self.mass - np.sum(self.dist))/count
+            #print('Mass Loss' , np.sum(self.dist) - self.mass)
         row_local, col_local = np.where(self.dist > threshold)  # Zellen die nicht im threshold liegen müssen ihre masse auf die anderen verteilen!
         
         return self.rowindex - 1 + row_local, self.colindex - 1 + col_local, self.dist[row_local, col_local], self.kin_energy_neighbour[row_local, col_local]
@@ -131,10 +150,11 @@ class Cell():
 ## Programm:        
 def get_start_idx(release):
     row_list, col_list = np.where(release > 0)  # Gives back the indices of the release areas
-    altitude_list = []
-    for i in range(len(row_list)):
-        altitude_list.append(dem[row_list[i], col_list[i]])    
-    altitude_list, row_list, col_list = list(zip(*sorted(zip(altitude_list, row_list, col_list), reverse=True)))  #Sort this lists by altitude
+    if len(row_list) > 0:
+        altitude_list = []
+        for i in range(len(row_list)):
+            altitude_list.append(dem[row_list[i], col_list[i]])    
+        altitude_list, row_list, col_list = list(zip(*sorted(zip(altitude_list, row_list, col_list), reverse=True)))  #Sort this lists by altitude
     return row_list, col_list   
 
 # =============================================================================
@@ -150,14 +170,26 @@ def get_start_idx(release):
 path = '/home/neuhauser/git_rep/graviclass/'
 file = path + 'dhm.asc'
 release_file = path + 'class_1.asc'
+# =============================================================================
+# path = '/home/P/Projekte/18130-GreenRisk4Alps/Simulation/PAR3_Oberammergau/'
+# file = path + 'DEM_10_3.tif'
+# release_file = path + 'init/release_class_1.asc'
+# =============================================================================
 elh_out = path + 'energy_flowr.asc'
 mass_out = path + 'mass_flowr.asc'
+index_out = path + 'index_flowr.asc'
+
+#dem = np.array([[920,915,920], [910,900,910], [880, 890, 885]])
+#cellsize = 10
+#release = np.array([[0,0,0], [0,1,0], [0,0,0]])
 
 dem, header = io.read_raster(file)
-cellsize = header["cellsize"]   
+cellsize = header["cellsize"]  
 release, header = io.read_raster(release_file) 
+
 elh = np.zeros_like(dem)
 mass_array = np.zeros_like(dem)
+index_array = np.zeros_like(dem)
 
 
 start = time.time()
@@ -167,7 +199,9 @@ row_list, col_list = get_start_idx(release)
 
 startcell_idx = 0
 while startcell_idx < len(row_list):
-    print("Calculating Startcell: " + str(startcell_idx+1) + " of " + str(len(row_list)) + " = " + str(round(startcell_idx/len(row_list)*100,2)) + "%")
+    sys.stdout.write('\r' "Calculating Startcell: " + str(startcell_idx+1) + " of " + str(len(row_list)) + " = " + str(round(startcell_idx+1/len(row_list)*100,2)) + "%" '\r')
+    sys.stdout.flush()
+    #print("Calculating Startcell: " + str(startcell_idx+1) + " of " + str(len(row_list)) + " = " + str(round(startcell_idx/len(row_list)*100,2)) + "%")
     cell_list = []
     row_idx = row_list[startcell_idx]
     col_idx = col_list[startcell_idx]    
@@ -176,23 +210,22 @@ while startcell_idx < len(row_list):
         startcell_idx += 1
         continue
     
-    startcell = Cell(row_idx, col_idx, dem_ng, cellsize, 1, 0, None, startcell=True)
+    startcell = Cell(row_idx, col_idx, dem_ng, cellsize, 1, 15, None, startcell=True)
     # If this is a startcell just give a Bool to startcell otherwise the object startcell
     
     cell_list.append(startcell)
     checked = 0
+    index = 0
     for cells in cell_list:
-        #if cells.kin_e < 0 or cells.mass < 1e-20:
-            #checked += 1
-            #continue
         row, col, mass, kin_e = cells.calc_distribution()
 
-        for i in range(len(cell_list)):  # Check if Cell already exists
+        for i in range(int(checked), len(cell_list)):  # Check if Cell already exists
             k = 0
             while k < len(row):
                 if (row[k] == cell_list[i].rowindex and col[k] == cell_list[i].colindex):
                     cell_list[i].add_mass(mass[k])
                     cell_list[i].add_parent(cells)
+                    cell_list[i].calc_direction()
                     row = np.delete(row, k)
                     col = np.delete(col, k)
                     mass = np.delete(mass, k)
@@ -209,6 +242,8 @@ while startcell_idx < len(row_list):
         #checked += 1         
         elh[cells.rowindex, cells.colindex] = max(elh[cells.rowindex, cells.colindex], cells.kin_e)
         mass_array[cells.rowindex, cells.colindex] = cells.mass
+        index_array[cells.rowindex, cells.colindex] = index
+        index += 1
     release[elh > 0] = 0  # Check if i hited a release Cell, if so set it to zero and get again the indexes of release cells
 
     row_list, col_list = get_start_idx(release)
@@ -220,3 +255,4 @@ print('Time needed: ' + str(end - start) + ' seconds')
 # Output
 io.output_raster(file, elh_out, elh, 4326)
 io.output_raster(file, mass_out, mass_array, 4326)
+io.output_raster(file, index_out, index_array, 4326)
